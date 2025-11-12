616be32391e6ce155a2eeb0cbbbbe83de6dbd67c
Nathaniel Wright <nathaniel.wright@aveva.com>
2025-11-12 10:56:33 -0800

    Cleanup

--- tests/AVEVA/RocksDB/Plugin/Azure/Impl/BufferChunkInfoTests.cpp
+++ tests/AVEVA/RocksDB/Plugin/Azure/Impl/BufferChunkInfoTests.cpp
@@ -2,49 +2,46 @@
 
 #include <gtest/gtest.h>
 
-using namespace AVEVA::RocksDB::Plugin::Azure::Impl;
-
-class BufferChunkInfoTests : public ::testing::Test {};
-
-TEST_F(BufferChunkInfoTests, ChunkSize_ReturnsCorrectTotal) {
+using AVEVA::RocksDB::Plugin::Azure::Impl::BufferChunkInfo;
+TEST(BufferChunkInfoTests, ChunkSize_ReturnsCorrectTotal) {
     // Arrange
-    BufferChunkInfo chunk(0, 512, 100, 12, 400);
+    BufferChunkInfo chunk{ 0, 512, 100, 12, 400 };
 
     // Act
-    auto size = chunk.ChunkSize();
+    const auto size = chunk.ChunkSize();
 
     // Assert
     EXPECT_EQ(512, size); // 12 + 100 + 400
 }
 
-TEST_F(BufferChunkInfoTests, ChunkSize_WithNoPadding_ReturnsDataLength) {
+TEST(BufferChunkInfoTests, ChunkSize_WithNoPadding_ReturnsDataLength) {
     // Arrange
-    BufferChunkInfo chunk(0, 0, 512, 0, 0);
+    BufferChunkInfo chunk{ 0, 0, 512, 0, 0 };
 
     // Act
-    auto size = chunk.ChunkSize();
+    const auto size = chunk.ChunkSize();
 
     // Assert
     EXPECT_EQ(512, size);
 }
 
-TEST_F(BufferChunkInfoTests, ChunkSize_WithOnlyPrePadding) {
+TEST(BufferChunkInfoTests, ChunkSize_WithOnlyPrePadding) {
     // Arrange
-    BufferChunkInfo chunk(0, 100, 412, 100, 0);
+    BufferChunkInfo chunk{ 0, 100, 412, 100, 0 };
 
     // Act
-    auto size = chunk.ChunkSize();
+    const auto size = chunk.ChunkSize();
 
     // Assert
     EXPECT_EQ(512, size); // 100 + 412 + 0
 }
 
-TEST_F(BufferChunkInfoTests, ChunkSize_WithOnlyPostPadding) {
+TEST(BufferChunkInfoTests, ChunkSize_WithOnlyPostPadding) {
     // Arrange
-    BufferChunkInfo chunk(0, 0, 256, 0, 256);
+    BufferChunkInfo chunk{ 0, 0, 256, 0, 256 };
 
     // Act
-    auto size = chunk.ChunkSize();
+    const auto size = chunk.ChunkSize();
 
     // Assert
     EXPECT_EQ(512, size); // 0 + 256 + 256
