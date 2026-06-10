b60ea54daf05409e31928dde1ddb349eb38a379c
Jesus Naranjo <jesus.naranjo@aveva.com>
2026-06-10 16:48:00 -0700

    rename lambda parameter

--- src/AVEVA/RocksDB/Plugin/Azure/BlobFilesystem.cpp
+++ src/AVEVA/RocksDB/Plugin/Azure/BlobFilesystem.cpp
@@ -605,7 +605,7 @@ namespace AVEVA::RocksDB::Plugin::Azure
             }
 
             m_filesystem->UnlockFile(lockFile->GetImpl());
-            std::erase_if(m_lockFiles, [lockFile](const auto& l) { return l == lockFile; });
+            std::erase_if(m_lockFiles, [lockFile](const auto& entry) { return entry == lockFile; });
             delete lockFile;
             return rocksdb::IOStatus::OK();
         }
